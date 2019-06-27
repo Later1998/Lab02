953d37c1d65ae1df2ef6292677879743bd9dd507
Later1998 <Lawter.enemy1@gmail.com>
2019-06-27 18:37:05 +0700

    Create files

---
+++ src/Lab02.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class Lab02 {
+
+	public static void main(String args[]) {
+		double price = 10.5;
+		String name = "INW";
+		int[] num = {0,1,2,3,4,5,6,7};
+		
+        System.out.println("Hello World! : " + price);
+        for(int i=0; i<num.length ; i++)
+        	System.out.println("number : " + num[i]);
+	}
+
+}
